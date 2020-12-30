6b38dd34c21b1f27f48e854f0d630f83925c4dbe
mightnent <mightnent@gmail.com>
2020-12-30 17:04:42 +0800

    setting up front end

---
+++ listings/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+
+# Create your views here.
+def index(request):
+    return render(request,'listings/listings.html')
+
+def single_listing(request):
+    return render(request,'listings/single_listing.html')
+
+def search(request):
+    return render(request,'listings/search.html')
