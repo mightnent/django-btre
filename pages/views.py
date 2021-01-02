a84fe50fd9cda21995e12c880a28aed9b9a75df8
mightnent <mightnent@gmail.com>
2021-01-02 11:20:28 +0800

    home and about page

--- pages/views.py
+++ pages/views.py
@@ -1,9 +1,21 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from listings.models import Single_Listing
+from realtors.models import Realtor
 
 # Create your views here.
 def index(request):
-    return render(request,'pages/index.html')
+    Listings = Single_Listing.objects.order_by('-list_date').filter(is_published=True)[:3]
+    context = {
+        'Listings':Listings
+    }
+    return render(request,'pages/index.html',context)
 
 def about(request):
-    return render(request,'pages/about.html')
+    Realtors = Realtor.objects.order_by('-hire_date')
+    MVP = Realtor.objects.all().filter(is_mvp=True)
+    context = {
+        'Realtors' : Realtors,
+        'MVP' : MVP
+    }
+    return render(request,'pages/about.html',context)
--- realtors/admin.py
+++ realtors/admin.py
@@ -4,8 +4,9 @@ from django.contrib import admin
 from .models import Realtor
 
 class Realtor_Admin(admin.ModelAdmin):
-    list_display = ('id','name','email','hire_date')
+    list_display = ('id','name','is_mvp','email','hire_date')
     list_display_links = ('id','name')
     search_fields = ('name','email')
+    list_editable = ('is_mvp',)
     list_per_page = 25
 admin.site.register(Realtor,Realtor_Admin)
