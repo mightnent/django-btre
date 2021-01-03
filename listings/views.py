7d71d649e9f6d3b613eb4a2dd0e3b9316e603518
mightnent <mightnent@gmail.com>
2021-01-03 15:17:15 +0800

    single page listing

--- listings/views.py
+++ listings/views.py
@@ -1,10 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404,render
 from django.core.paginator import EmptyPage,PageNotAnInteger,Paginator
 from .models import Single_Listing
 
 # Create your views here.
 def index(request):
-    Listings = Single_Listing.objects.order_by(-'list_date').filter(is_published=True)
+    Listings = Single_Listing.objects.order_by('-list_date').filter(is_published=True)
     paginator = Paginator(Listings,3)
     page_number = request.GET.get('page')
     page_obj = paginator.get_page(page_number)
@@ -15,7 +15,12 @@ def index(request):
 
 #listing_id comes from urls.py of listing
 def single_listing(request,listing_id):
-    return render(request,'listings/single_listing.html')
+    listing = get_object_or_404(Single_Listing,pk=listing_id)
+   
+    context = {
+        'listing':listing
+    }
+    return render(request,'listings/single_listing.html',context)
 
 def search(request):
     return render(request,'listings/search.html')
