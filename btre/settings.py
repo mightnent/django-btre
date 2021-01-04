5668540f18f9212205388b69d91b379eb83e3f8f
mightnent <mightnent@gmail.com>
2021-01-04 15:38:29 +0800

    init accounts app

--- btre/settings.py
+++ btre/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'accounts.apps.AccountsConfig',
     'pages.apps.PagesConfig',
     'listings.apps.ListingsConfig',
     'realtors.apps.RealtorsConfig',
