f8a095f0f19008e52812dd4a82dfa165e0d7fc5c
mightnent <mightnent@gmail.com>
2021-01-05 21:50:24 +0800

    alerts and registration

--- accounts/views.py
+++ accounts/views.py
@@ -1,9 +1,34 @@
 from django.shortcuts import render,redirect
+from django.contrib import messages,auth
+from django.contrib.auth.models import User
 
 # Create your views here.
 def register(request):
     if request.method == "POST":
-        pass
+        #get form values from POST
+        first_name = request.POST['first_name']
+        last_name = request.POST['last_name']
+        username = request.POST['username']
+        email = request.POST['email']
+        password = request.POST['password']
+        password2 = request.POST['password2']
+        if password == password2:
+            if User.objects.filter(username=username).exists():
+                messages.error(request,"Username Taken")
+                return redirect('register')
+            if User.objects.filter(email=email).exists():
+                messages.error(request,"Email Taken")
+                return redirect('register')
+            #things seems to be ok
+            user = User.objects.create_user(username=username,password=password,email=email,first_name=first_name,last_name=last_name)
+            #login after successful registration
+            # auth.login(request,user)
+            user.save()
+            messages.success(request,"You are registered")
+            return redirect('login')
+        else:
+            messages.error(request,'Passwords did not match!')
+            return redirect('register')
     else:
         return render(request,'accounts/register.html')
 
--- btre/settings.py
+++ btre/settings.py
@@ -135,3 +135,10 @@ STATICFILES_DIRS = [
 #media folder
 MEDIA_ROOT = os.path.join(BASE_DIR,'media')
 MEDIA_URL = '/media/'
+
+#messages
+from django.contrib.messages import constants as messages
+MESSAGE_TAGS = {
+    messages.ERROR: 'danger',
+   
+}
