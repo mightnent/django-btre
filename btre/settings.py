30d3d70fed8bd6919aecfca93b0633e1efd3c487
mightnent <mightnent@gmail.com>
2021-01-09 21:04:08 +0800

    remove email

--- btre/settings.py
+++ btre/settings.py
@@ -144,12 +144,6 @@ MESSAGE_TAGS = {
    
 }
 
-#email
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_HOST_USER = 'xxx@gmail.com'
-EMAIL_HOST_PASSWORD = 'yyy'
-EMAIL_PORT = 587
-EMAIL_USE_TLS = True
 
 #.local_setting is actually for production. It is call local by convention
 try:
