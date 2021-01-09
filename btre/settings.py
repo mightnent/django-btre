33d251bc08a1590c7aad12e32e08fc4186e60af6
mightnent <mightnent@gmail.com>
2021-01-09 17:35:26 +0800

    send_mail for inquiry

--- btre/settings.py
+++ btre/settings.py
@@ -143,3 +143,10 @@ MESSAGE_TAGS = {
     messages.ERROR: 'danger',
    
 }
+
+#email
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST_USER = 'xxx@gmail.com'
+EMAIL_HOST_PASSWORD = 'yyy'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
--- contacts/views.py
+++ contacts/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render,redirect
 from django.contrib import messages
+from django.core.mail import send_mail
 from .models import Contact
 
 
@@ -16,9 +17,27 @@ def contact(request):
         user_id = request.POST['user_id']
         realtor_email = request.POST['realtor_email'] 
 
+        #check if user has already made an inquiry on the same listing
+        if request.user.is_authenticated:
+            #user_id = request.user_id
+            has_contacted = Contact.objects.all().filter(listing_id=listing_id,user_id=user_id)
+            if has_contacted:
+                messages.error(request,"A prior inquiry has been made")
+                return redirect('/listings/'+listing_id)
+
+
+
         contact = Contact(listing=listing,listing_id=listing_id,name=name,email=email,phone=phone,message=message,user_id=user_id)
         contact.save()
+
+        send_mail(
+            'Property listing inquiry',
+            'There has been an inquiry on '+listing+'. You can log into admin panel to check it out.',
+            'xxx@gmail.com',
+            [realtor_email,"xxx@gmail.com"],
+            fail_silently=False
+        )
+
         messages.success(request,"Submitted!")
 
-        print("hello")
         return redirect('/listings/'+listing_id)
